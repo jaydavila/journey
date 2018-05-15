51e6834b94a83f6771271e5d986f05d262d881fc
Jay Davila <jaydavila94@icloud.com>
2018-05-15 15:55:34 -0400

    created post model and declares rescource Close #2, Closes #3, Closes #4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_184344) do
+ActiveRecord::Schema.define(version: 2018_05_15_194913) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
