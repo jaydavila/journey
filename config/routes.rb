39ff863d9f75b4731c8eb89c62bdb6a47ec35845
Jay Davila <jaydavila94@icloud.com>
2018-05-16 12:26:01 -0400

    made controller user and added references to comment/posts migratons

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'comments/index'
-  get 'comments/show'
-  get 'comments/new'
-  get 'comments/create'
-  get 'comments/edit'
-  get 'comments/update'
-  get 'comments/destroy'
   devise_for :users
 
   root 'posts#index', as: 'home'
-  resources :users
+  resources :users, only: [:index, :show]
   resources :posts do
    resources :comments
  end
--- db/schema.rb
+++ db/schema.rb
@@ -16,16 +16,20 @@ ActiveRecord::Schema.define(version: 2018_05_16_001417) do
     t.string "name"
     t.text "body"
     t.integer "post_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
