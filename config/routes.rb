6d214f492756699620e9f412a2168605b647a0e7
Jay Davila <jaydavila94@icloud.com>
2018-05-15 20:42:45 -0400

    generated comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/edit'
+  get 'comments/update'
+  get 'comments/destroy'
   devise_for :users
 
   root 'posts#index', as: 'home'
   resources :users
-  resources :posts, :except => [:show]
-  get '/posts/:id' => 'posts#showpost'
+  resources :posts do
+   resources :comments
+ end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
