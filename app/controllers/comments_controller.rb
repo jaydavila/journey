b442c723aad0c887ee7e57b0385161faca64e8aa
Jay Davila <jaydavila94@icloud.com>
2018-05-17 15:46:36 -0400

    This needs some styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,8 +10,12 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = @post.id
+    @comment.save
     redirect_to post_path(@post)
+
   end
 
   def edit
