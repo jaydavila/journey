390f1d03b2f734a5c14d8ed750f26a49203fa117
Jay Davila <jaydavila94@icloud.com>
2018-05-17 12:46:48 -0400

    updates

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   belongs_to :user
-  
+
   validates :title, presence: true,
                       length:{minimum: 6}
 
